2f8082c4cb8c82d3fe75ad280271cf10a7178905
Ray Arvin Rimorin <ray@fbmsoftware.com>
2024-11-14 14:04:44 +0800

    remove geocode from map component

--- src/components/MapView/index.tsx
+++ src/components/MapView/index.tsx
@@ -1,53 +1,39 @@
-import { APIProvider, APIProviderContext, Map, MapMouseEvent, useMap, useMapsLibrary } from '@vis.gl/react-google-maps';
-import { useCallback, useEffect, useState } from 'react';
+import { APIProvider, Map, MapMouseEvent } from '@vis.gl/react-google-maps';
+import { useCallback, useState } from 'react';
+import { InfoWindowMarker } from './InfoWindowMarker';
 
 const MapView = () => {
 
-  const geocodingLibrary = useMapsLibrary('geocoding')
-  const map = useMap()
+  const [currentMarker, setCurrentMarker] = useState<google.maps.LatLngLiteral | null>();
 
-  const [geocodingService, setGeocodingService] = useState<google.maps.Geocoder>();
-
-  useEffect(
-    () => {
-      if (!geocodingLibrary || !map) return
-      setGeocodingService(new geocodingLibrary.Geocoder())
-    }, [geocodingLibrary, map]
-  );
 
   const handleMapClick = useCallback(
     async (ev: MapMouseEvent) => {
-      try {
-        const geocodeResult = await geocodingService?.geocode({location: ev.detail.latLng})
-        console.log('index.tsx (21)', geocodeResult)
-      } catch (error) {
-        console.error(error)
-      }
-
-    }, [geocodingService]
+      setCurrentMarker(ev.detail.latLng)
+    }, []
   );
 
   return (
-    
-      <Map
-        style={{ width: '100%', height: '100%' }}
-        defaultCenter={{
-          lat: -37.4502222,
-          lng: 144.5742387
-        }}
-        defaultZoom={13}
-        gestureHandling={"greedy"}
-        disableDefaultUI={false}
-        onClick={handleMapClick}
-      >
-        
-      </Map>
+
+    <Map
+      style={{ width: '100%', height: '100%' }}
+      defaultCenter={{
+        lat: -37.4502222,
+        lng: 144.5742387
+      }}
+      defaultZoom={13}
+      gestureHandling={"greedy"}
+      disableDefaultUI={false}
+      onClick={handleMapClick}
+    >
+      <InfoWindowMarker latLng={currentMarker} />
+    </Map>
   )
 }
 
 const MapViewContainer = () => (
   <APIProvider apiKey={import.meta.env.VITE_MAPBOX_TOKEN}>
-    <MapView/>
+    <MapView />
   </APIProvider>
 )
 
